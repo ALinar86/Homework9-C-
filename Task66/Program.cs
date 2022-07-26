4d746554b90f427860c9a4b7c6192d909ea68f56
Linar <alr86@mail.ru>
2022-07-26 06:10:00 +0300

    Added 66

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿int ShowSumOfDigits(int n, int m)
+{
+    if (n < m)
+    {
+        return n + ShowSumOfDigits(n + 1, m);
+    }
+    else return n;
+}
+
+Console.Write("Input a natural number 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a natural number 2: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"The sum of digits is {ShowSumOfDigits(n, m)}");
+Console.WriteLine(ShowSumOfDigits(n, m));
