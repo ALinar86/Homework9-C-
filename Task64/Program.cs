03f7be69fc521b04c3fa4841e3de4b8ac188c6b7
Linar <alr86@mail.ru>
2022-07-26 06:08:47 +0300

    Added 64

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿int NumOfDigit(int n)
+{
+    if (n >= 10)
+    {
+        return 1 + NumOfDigit(n / 10);
+    }
+    else if (n == 0)
+    {
+        return 1;
+    }
+
+    else return n / n;
+}
+
+Console.Write("Input a natural number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"The number of digits is {NumOfDigit(n)}");
